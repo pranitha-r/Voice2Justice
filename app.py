7e6b6245e0123a159aabc77869f0686c89356d18
pranitha-r <rpranitha909@gmail.com>
2026-03-06 22:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -415,6 +415,8 @@ if __name__ == '__main__':
     print("  Voice2Justice Flask Server")
     print("  Open http://127.0.0.1:5000 in your browser")
     print("=" * 50)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
 
 
