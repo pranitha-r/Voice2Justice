f269e0272087764c6ab113840096cbbd41ba45d5
pranitha-r <rpranitha909@gmail.com>
2026-03-06 22:16:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -416,7 +416,8 @@ if __name__ == '__main__':
     print("  Open http://127.0.0.1:5000 in your browser")
     print("=" * 50)
     port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
+
 
 
 
